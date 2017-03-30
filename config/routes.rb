b768c11bde93406870bb58d007be75745254a07c
falperezgo <falperezgo@unal.edu.co>
2017-03-30 00:42:38 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :juries
   resources :file_gradeworks
   resources :students
+  resources :administrators
+  resources :directors
+  
+  
   get 'contact_us/index'
 
   get 'home/index'
