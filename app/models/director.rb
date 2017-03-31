015bcf8d63801d336544f8a4bb775870d9157090
Leonardo <leoalbirojo@gmail.com>
2017-03-31 02:57:26 -0500

    1. Scopes Juries, directors and feedbacks

--- app/models/director.rb
+++ app/models/director.rb
@@ -2,6 +2,12 @@ class Director < ApplicationRecord
 	has_many :gradework
 	
 	validates :firstname, :lastname,  :phone, presence: true	
-	validates :email, :identification, presence: true, uniqueness: true	
+	validates :email, :identification, presence: true, uniqueness: true
+
+	scope :by_name, -> (firstname) { where firstname: firstname}
+	scope :by_lastname, -> (lastname) { where lastname: lastname}
+	scope :by_email, -> (email) { where email: email}
+	scope :by_phone, -> (phone) { where phone: phone}
+	scope :by_identification, -> (identification) { where identification: identification}
 	
 end
--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -3,4 +3,17 @@ class Feedback < ApplicationRecord
 	has_one :director
 
 	validates :score, presence: true
+
+	scope :pass, -> { where('score > 2') }
+	scope :lost, -> { where('score < 3')}
+	scope :by_director, ->(director_id) { where director_id: director_id }
+	scope :by_gradework, ->(gradework_id) { where gradework_id: gradework_id }
+  scope :by_score, ->(score) { where score: score}
+
+
+
+
+
+
+
 end
