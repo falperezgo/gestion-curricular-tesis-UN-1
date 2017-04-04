1b95bb668a8392687ad8561c502adf9d817fc3d2
Helmer Avendaño <haavendanov@unal.edu.co>
2017-04-03 23:49:46 -0500

    Fix front errors

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,35 @@ ActiveRecord::Schema.define(version: 20170402175216) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "administrators", force: :cascade do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "email"
+    t.string   "phone"
+    t.integer  "identification"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "directors", force: :cascade do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "email"
+    t.string   "phone"
+    t.integer  "identification"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "directors_gradeworks", force: :cascade do |t|
+    t.integer  "director_id"
+    t.integer  "gradework_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["director_id"], name: "index_directors_gradeworks_on_director_id", using: :btree
+    t.index ["gradework_id"], name: "index_directors_gradeworks_on_gradework_id", using: :btree
+  end
+
   create_table "feedbacks", force: :cascade do |t|
     t.text     "anotations"
     t.integer  "score"
@@ -59,6 +88,25 @@ ActiveRecord::Schema.define(version: 20170402175216) do
     t.datetime "updated_at",    null: false
   end
 
+  create_table "juries", force: :cascade do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "email"
+    t.string   "phone"
+    t.integer  "identification"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "juries_gradeworks", force: :cascade do |t|
+    t.integer  "jury_id"
+    t.integer  "gradework_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["gradework_id"], name: "index_juries_gradeworks_on_gradework_id", using: :btree
+    t.index ["jury_id"], name: "index_juries_gradeworks_on_jury_id", using: :btree
+  end
+
   create_table "role_users", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "role_id"
@@ -75,6 +123,16 @@ ActiveRecord::Schema.define(version: 20170402175216) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "students", force: :cascade do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "email"
+    t.string   "phone"
+    t.integer  "identification"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "firstname"
     t.string   "lastname"
@@ -85,11 +143,13 @@ ActiveRecord::Schema.define(version: 20170402175216) do
     t.datetime "updated_at",     null: false
   end
 
+  add_foreign_key "directors_gradeworks", "directors"
   add_foreign_key "feedbacks", "gradeworks"
   add_foreign_key "feedbacks", "users"
   add_foreign_key "file_gradeworks", "gradeworks"
   add_foreign_key "gradework_users", "gradeworks"
   add_foreign_key "gradework_users", "users"
+  add_foreign_key "juries_gradeworks", "juries"
   add_foreign_key "role_users", "roles"
   add_foreign_key "role_users", "users"
 end
