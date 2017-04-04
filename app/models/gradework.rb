b012a896626d2c8aae7b267d3eca536c9a597a3f
Leonardo <leoalbirojo@gmail.com>
2017-04-03 23:55:24 -0500

    Gradework queries

--- app/models/gradework.rb
+++ app/models/gradework.rb
@@ -6,4 +6,55 @@ class Gradework < ApplicationRecord
 
   validates :name, :status, :delivery_date, :begin_date, :hour, :locale, :semester, presence: true
 
+  default_scope {order("gradeworks.name")}
+  scope :order_by_name, -> (ord) {order("gradeworks.name #{ord}")}
+  scope :order_by_semester, -> (ord) {order("gradeworks.semester #{ord}")}
+  scope :order_by_delivery_date, -> (ord) {order("gradeworks.delivery_date #{ord}")}
+  scope :order_by_begin_date, -> (ord) {order("gradeworks.begin_date #{ord}")}
+  scope :order_by_created_at, -> (ord) {order("gradeworks.created_at #{ord}")}
+
+#def self.load_gradeworks(**args)
+#  paginate(:page => args[:page] || 1,:per_page => args[:per_page] || 10)
+#end
+
+  def self.gradeworks_by_id(id)
+    find_by_id(id)
+  end
+
+  def self.gradeworks_by_ids(ids,**args)
+#  load_admins(args)
+    self
+        .where(gradeworks: {
+            id: ids
+        })
+  end
+
+  def self.gradeworks_by_not_ids(ids,**args)
+#  load_admins(args)
+    self
+        .where.not(gradeworks: {
+        id: ids
+    })
+  end
+
+  def self.gradeworks_by_name(name)
+    find_by_name(name)
+  end
+
+  def self.gradeworks_student(id)
+    includes(users: [:roles]).where({name: "Student"})
+  end
+
+  def self.gradeworks_administrator(id)
+    includes(users: [:roles]).where({name: "Administator"})
+  end
+
+  def self.gradeworks_director(id)
+    includes(users: [:roles]).where({name: "Teacher"})
+  end
+
+  def self.gradeworks_jury(id)
+    includes(users: [:roles]).where({name: "Jury"})
+  end
+
 end
